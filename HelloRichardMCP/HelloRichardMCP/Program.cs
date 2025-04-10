cc188127dc2e9b3685448f77ed5730866b47123b
Richard Hunter <r-hunter@live.co.uk>
2025-04-10 21:04:32 +0100

    Initial commit

---
+++ HelloRichardMCP/HelloRichardMCP/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+    // Configure all logs to go to stderr
+    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+
+await builder.Build().RunAsync();
+
+
+[McpServerToolType]
+public static class GreetingTool
+{
+    [McpServerTool, Description("Greets Richard.")]
+    public static string GreetRichard() => "HELLO RICHARD";
+}
